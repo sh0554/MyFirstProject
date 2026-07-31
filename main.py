76a7d473a2a1168742dd881f067e5c542478bebf
sh0554 <ssd1575@naver.com>
2026-07-31 14:14:54 +0900

    Python 계산기 프로그램 추가

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-number = int(input("몇 단을 출력할까요? "))
+def add(a, b):
+    return a + b
 
-for i in range(1, 10):
-    print(number, "×", i, "=", number * i)
-    def gugudan(number):
-    for i in range(1, 10):
-        print(number, "×", i, "=", number * i)
 
-gugudan(7)
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+print("===== Python 계산기 =====")
+
+num1 = int(input("첫 번째 숫자: "))
+operator = input("연산자 입력 (+ - * /): ")
+num2 = int(input("두 번째 숫자: "))
+
+
+if operator == "+":
+    result = add(num1, num2)
+
+elif operator == "-":
+    result = subtract(num1, num2)
+
+elif operator == "*":
+    result = multiply(num1, num2)
+
+elif operator == "/":
+    result = divide(num1, num2)
+
+else:
+    result = "지원하지 않는 연산자입니다."
+
+
+print("결과:", result)
