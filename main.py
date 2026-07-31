a19c68550d0fb90cd13d5c259b3abf2778640ef9
sh0554 <ssd1575@naver.com>
2026-07-31 10:47:00 +0900

    구구단 프로그램 추가

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ number = int(input("몇 단을 출력할까요? "))
 
 for i in range(1, 10):
     print(number, "×", i, "=", number * i)
+    def gugudan(number):
+    for i in range(1, 10):
+        print(number, "×", i, "=", number * i)
+
+gugudan(7)
