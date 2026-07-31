342565f037c37f59f12440b35f2febb5e15a5258
sh0554 <ssd1575@naver.com>
2026-07-31 09:49:28 +0900

    Python 첫 번째 프로그램

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("안녕하세요! Python 개발 환경 성공!")
+name = input("이름을 입력하세요: ")
+age = input("나이를 입력하세요: ")
+
+print()
+print("===== 자기소개 =====")
+print("이름:", name)
+print("나이:", age)
+print("만나서 반갑습니다!")
